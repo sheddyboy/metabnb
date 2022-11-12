f425a9e8dbba6093eebef5aef0ccb40ae76a9162
Shadrack Itulah <72614183+sheddyboy@users.noreply.github.com>
2022-11-12 13:26:51 +0100

    chore: Add cta section

---
+++ src/layout/CTA.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "../styles/cta.module.css";
+
+const CTA = () => {
+  return (
+    <section className={styles.section}>
+      <div className="container">
+        <div className={styles.wrapper}>
+          <div className={styles.content}>
+            <h2>Metabnb NFTs</h2>
+            <p>
+              Discover our NFT gift cards collection. Loyal customers gets
+              amazing gift cards which are traded as NFTs. These NFTs gives our
+              cutomer access to loads of our exclusive services.
+            </p>
+            <button>
+              <span>Learn more</span>
+            </button>
+          </div>
+          <div className={styles.image}>
+            <img src="/cta_image.svg" alt="cta" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CTA;
