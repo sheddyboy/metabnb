787201ff0fe5e7a27bd43b65c98e26e8aed5c94f
Shadrack Itulah <72614183+sheddyboy@users.noreply.github.com>
2022-11-13 18:03:13 +0100

    chore: Add mobile menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,57 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import styles from "../styles/nav.module.css";
 import { Link } from "react-router-dom";
 import { ModalCtx } from "../contexts/ModalContex";
 
 const Nav = () => {
+  const [mobilenav, setMobilenav] = useState(false);
   const { toggleModal } = useContext(ModalCtx);
 
   return (
-    <nav className={styles.nav + " container"}>
-      <img src="/nav_logo.svg" alt="logo" />
-      <div className={styles.nav_links}>
-        <Link to="/">Home</Link>
-        <Link to="/">Place to stay</Link>
-        <Link to="/nfts">NFTs</Link>
-        <Link to="/">Community</Link>
+    <nav className={styles.nav}>
+      <div className="container">
+        <div className={styles.wrapper}>
+          <img src="/nav_logo.svg" alt="logo" />
+          <div className={styles.nav_links}>
+            <Link to="/">Home</Link>
+            <Link to="/">Place to stay</Link>
+            <Link to="/nfts">NFTs</Link>
+            <Link to="/">Community</Link>
+          </div>
+          <button className={styles.btn} onClick={toggleModal}>
+            Connect wallet
+          </button>
+          <div
+            className={styles.hamMenu}
+            onClick={() => {
+              setMobilenav((mobilenav) => !mobilenav);
+            }}
+          >
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          {mobilenav && (
+            <div className={styles.mobileNav}>
+              <div className={styles.mobileNavLinks}>
+                <Link to="/">Home</Link>
+                <Link to="/">Place to stay</Link>
+                <Link to="/nfts">NFTs</Link>
+                <Link to="/">Community</Link>
+                <button className={styles.mobileBtn} onClick={toggleModal}>
+                  Connect wallet
+                </button>
+              </div>
+              <div
+                className={styles.overlay}
+                onClick={() => {
+                  setMobilenav((mobilenav) => !mobilenav);
+                }}
+              ></div>
+            </div>
+          )}
+        </div>
       </div>
-      <button onClick={toggleModal}>Connect wallet</button>
     </nav>
   );
 };
