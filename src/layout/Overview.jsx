e47cab2fa950bc6e5f924d22f648b6432a56db41
Shadrack Itulah <72614183+sheddyboy@users.noreply.github.com>
2022-11-13 12:12:39 +0100

    chore: move list of nfts to a new file

--- src/layout/Overview.jsx
+++ src/layout/Overview.jsx
@@ -1,26 +1,17 @@
 import React from "react";
 import Card from "../components/Card";
+import { nfts } from "../nfts";
 import styles from "../styles/overview.module.css";
 
 const Overview = () => {
-  const nfts = [
-    { image: "/nft_1.svg", id: "1" },
-    { image: "/nft_2.svg", id: "2" },
-    { image: "/nft_3.svg", id: "3" },
-    { image: "/nft_4.svg", id: "4" },
-    { image: "/nft_5.svg", id: "5" },
-    { image: "/nft_6.svg", id: "6" },
-    { image: "/nft_7.svg", id: "7" },
-    { image: "/nft_8.svg", id: "8" },
-  ];
   return (
     <section className={styles.section}>
       <div className="container">
         <h2>Inspiration for your next adventure</h2>
         <div className={styles.grid}>
-          {nfts.map((i) => (
-            <Card key={i.id} image={i.image} />
-          ))}
+          {nfts.map(
+            (i, index) => index < 8 && <Card key={i.id} image={i.image} />
+          )}
         </div>
       </div>
     </section>
